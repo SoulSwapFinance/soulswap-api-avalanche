c7cd9790a10f28bd0e566052a33b120bd22ab4d0
Buns <bunsthedev@gmail.com>
2021-11-28 11:15:53 -0600

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import express, { Express } from "express";
 import http from "http";
 import morgan from "morgan";
-const cors = require('@koa/cors');
-
 import routes from "./routes/main";
 const router: Express = express();
 
 router.use(morgan("dev"));
 router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
-router.use(cors({ origin: '*' }));
-router.use(router.routes());
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+router.use("/", routes);
 
 router.use((req, res, next) => {
   const error = new Error("not found");
